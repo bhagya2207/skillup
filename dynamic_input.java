e9aba938405247b5227504232932b148add086e1
bhagya2207 <69617463+bhagya2207@users.noreply.github.com>
2020-08-14 11:24:46 +0530

    Add files via upload

---
+++ dynamic_input.java
@@ -0,0 +1,21 @@
+package pack1;
+import java.util.*;
+
+public class dynamic_input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner obj = new Scanner(System.in);
+		//instantiation or object creation
+		
+		System.out.println("Enter first number");
+		int a = obj.nextInt();
+		
+		System.out.println("Enter second number");
+		int b = obj.nextInt();
+		
+		int c=a+b;
+		System.out.println("Addition is " +c);
+	}
+
+}
