bb4b45584de3afe622dbedb420d604fd986b3036
bhagya2207 <69617463+bhagya2207@users.noreply.github.com>
2020-08-17 08:58:45 +0530

    Add files via upload

---
+++ pentagon.java
@@ -0,0 +1,9 @@
+package pack2;
+public class pentagon {
+	public static void main(String[] args) {
+		int s=13;
+		int a=5;
+		float area_pentagon=(float)(5.0/2.0)*s*a;
+		System.out.println("Area of the Pentagon "+area_pentagon);
+	}
+}
