a03f314653a79268b0231578e6ed3802fbe1c742
bhagya2207 <69617463+bhagya2207@users.noreply.github.com>
2020-08-17 08:56:57 +0530

    Add files via upload

---
+++ do_while1.java
@@ -0,0 +1,12 @@
+package pack2;
+public class do_while1 {
+	public static void main(String[] args) {
+		int i=1;
+		int n=5;
+		do 
+		{
+			System.out.println(n+"*"+i+"="+(n*i));
+			i++;
+		}while(i<=10);
+	}
+}
