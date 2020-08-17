ac124ea2b8e6a6e25711f0708390e63aede990a2
bhagya2207 <69617463+bhagya2207@users.noreply.github.com>
2020-08-17 08:57:54 +0530

    Add files via upload

---
+++ mul2.java
@@ -0,0 +1,15 @@
+package pack2;
+public class mul2 {
+	public static void main(String[] args) {
+		for(int j=0;j<=5;j++) 
+		{
+			for (int i=1;i<=j;i++) 
+			{
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
