9d5699696b66db798637b1dddf8d821d835cca12
bhagya2207 <69617463+bhagya2207@users.noreply.github.com>
2021-05-14 09:36:38 +0530

    Add files via upload

---
+++ 14 may.py
@@ -0,0 +1,29 @@
+#print max and min
+n=int(input("Enter Number: "))
+mi=9
+ma=0
+while n:
+    r=n%10
+    n=n//10
+    if r>ma:
+        ma=r
+    if r<mi:
+        mi=r
+print(mi,ma)
+#number of max and min terms
+n=int(input("Enter Number: "))
+mi=9
+ma=0
+c=d=0
+while n:
+    r=n%10
+    n=n//10
+    if r>ma:
+        ma=r
+    elif r==ma:
+        c+=1
+    if r<mi:
+        mi=r
+    elif r==mi:
+        d+=1
+print(mi,ma,c,d)
