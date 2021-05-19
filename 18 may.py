7f500b407c24127d2b277c6307d83b56972f77d2
bhagya2207 <69617463+bhagya2207@users.noreply.github.com>
2021-05-19 09:19:35 +0530

    Update 18 may.py

--- 18 may.py
+++ 18 may.py
@@ -1,28 +1,5 @@
-#position of fibanocci number
-"""def fib(num):
-    a,b,c,=0,1,0
-    count=2
-    if num==0:
-        print(1)
-        return
-    if num==1:
-        print(2)
-        return
-    
-    while c<=num:
-       c=a+b
-       a=b
-       b=c
-       count+=1
-       if c==num:
-          print("count=",count)
-          break
-    else:
-       print(False)
-num=int(input())
-fib(num)"""
 #multiplication sum
-"""a,b=map(int,input().split())
+a,b=map(int,input().split())
 sum=0
 while True:
     if a%2:
@@ -31,9 +8,10 @@ while True:
     b=b*2
     if a==0:
         break      
-print(sum)"""
+print(sum)
+
 # above program using functions
-"""def mul(a,b):
+def mul(a,b):
     sum=0
 while True:
     if a%2:
@@ -41,7 +19,8 @@ while True:
     a=a//2
     b=b*2     
 print(sum)
-a,b=map(int,input().split())"""
+a,b=map(int,input().split())
+
 #number series
 n=int(input())
 while True:
