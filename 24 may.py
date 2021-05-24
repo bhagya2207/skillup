93b6f743fedfc884fd14e156d3949b30d41ba1c3
bhagya2207 <69617463+bhagya2207@users.noreply.github.com>
2021-05-24 09:39:30 +0530

    Add files via upload

---
+++ 24 may.py
@@ -0,0 +1,52 @@
+#pronic number
+n=int(input())
+f=0
+for i in range(1,n):
+    if n==i*(i+1):
+        f=1
+        break
+if f==1:
+    print("Pronic Number")
+else:
+    print("Not Pronic Number")
+#disurium number
+def dis(n):
+    count=0
+    a=0
+    temp=n
+    while n!=0:
+        
+        n=n//10
+        count+=1
+    n=temp
+    while temp!=0:
+        r=n%10
+        n=n//10
+        a=a+pow(r,count)
+        count=count-1
+        if a==temp:
+            print("Disurium")
+        else:
+            print("Not Disurium")
+    
+n=int(input())
+print(dis(n))
+#harshad number
+n=int(input())
+sum=0
+temp=n
+while temp>0:
+    r=temp%10
+    temp=temp//10
+    sum=sum+r
+if n%sum==0:
+    print("harshad")
+else:
+    print("Not harshad")
+
+
+
+
+
+
+    
